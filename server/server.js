674aeb0f01359539d36ca92409365bb894002941
Nazeeeh <nazihahmed55@gmail.com>
2021-05-05 11:57:19 +0100

    Third commit - User schema was created and connected database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan"); //logs request or show requests on the terminal 
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +10,15 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//Connect to mongoose database
+mongoose.connect("mongodb+srv://root:LGRJsiKbKRtKZVSx@amazon-redesign.imese.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+err => {
+    if (err){
+        console.log(err)
+    } else {
+        console.log("DATABASE CONNECTED SUCCESSFULLY");
+    }
+})
 
 //GET - Getting data from the server
 app.get('/', (req, res) => {
