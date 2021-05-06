ce39f3f6a5878098b934119cdc210279a604d484
Nazeeeh <nazihahmed55@gmail.com>
2021-05-06 13:51:38 +0100

    Added the POST UPDATE AND GET Apis

--- server/routes/product.js
+++ server/routes/product.js
@@ -30,13 +30,68 @@ router.post("/products", upload.single("photo"), async (req, res) => {
 
 
 // GET REQUEST - Get all products
+router.get('/products', async (req, res) => {
+
+    try {
+        let products = await Product.find();
+        res.json({
+            status: true,
+            products : products
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        }); 
+    }
+})
 
 
 //GET REQUEST - Get a single product
+router.get('/products/:id', async (req, res) => {
 
+    try {
+        let product = await Product.findOne({ _id: req.params.id});
+        res.json({
+            status: true,
+            product : product
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        }); 
+    }
+})
 
 // PUT REQUEST - Update a single product
+router.put('/products/:id', upload.single("photo"), async (req, res) => {
 
+    try {
+        let product = await Product.findOneAndUpdate({ _id: req.params.id}, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                category: req.body.categoryID,
+                photo: req.file.location,
+                owner: req.body.ownerID
+            },
+        }, {upsert: true}
+        
+        );
+
+        res.json({
+            status: true,
+            updatedProduct : product
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        }); 
+    }
+})
 
 //DELETE REQUEST - Delete a Product
 
